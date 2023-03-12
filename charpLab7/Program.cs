b5190fd04a543bdcede8a7a40cef8685924b1ce9
RadonRebirth <96480522+RadonRebirth@users.noreply.github.com>
2023-03-12 20:53:07 +0300

    Update Program.cs

--- charpLab7/Program.cs
+++ charpLab7/Program.cs
@@ -1,64 +1,69 @@
-﻿using System;
+using System;
 
 namespace charpLab7
 {
     abstract class Transport
     {
-        int speed;
-        int capacity;
-        int distance;
-        public Transport(int speed, int capacity, int distance)
-        {
-            this.speed = speed;
-            this.capacity = capacity;
-            this.distance = distance;
-        }
+        public abstract int speed { get; set; }
+        public abstract int capacity { get; set; }
+        public abstract int distance { get; set; }
         public void speedinf() => Console.WriteLine($"Скорость транспорта {speed}");
         public void capacityinf() => Console.WriteLine($"Скорость транспорта {capacity}");
         public void distanceinf() => Console.WriteLine($"Скорость транспорта {distance}");
     }
     class Car : Transport
     {
-        public Car(int speed, int capacity, int distance) : base(speed, capacity, distance) { }
+        public override int speed { get; set; }
+        public override int capacity { get; set; }
+        public override int distance { get; set; }
     }
     class Ship : Transport
     {
-        public Ship(int speed, int capacity, int distance) : base(speed, capacity, distance) { }
+        public override int speed { get; set; }
+        public override int capacity { get; set; }
+        public override int distance { get; set; }
     }
     class Airplane : Transport
     {
-        public Airplane(int speed, int capacity, int distance) : base(speed, capacity, distance) { }
+        public override int speed { get; set; }
+        public override int capacity { get; set; }
+        public override int distance { get; set; }
     }
     class bibika : Car
     {
-        int capacity;
-        int distance;
-        public bibika(int speed, int capacity, int distance) : base(speed, capacity, distance) { }
+        public bibika(int speed, int capacity, int distance){
+            this.speed = speed;
+            this.capacity = capacity;
+            this.distance = distance;
+        }
         public void print() => Console.WriteLine($"Ламборгини грузоподъёмностью {capacity} может проехать {distance}");
     }
     class lodka : Ship
     {
-        int capacity;
-        int distance;
-        public lodka(int speed, int capacity, int distance) : base(speed, capacity, distance) { }
-        public void print() => Console.WriteLine($"лодка грузоподъёмностью {capacity} может проехать {distance}");
+        public lodka(int speed, int capacity, int distance){
+            this.speed = speed;
+            this.capacity = capacity;
+            this.distance = distance;
+        }
+        public void print() => Console.WriteLine($"Лодка грузоподъёмностью {capacity} может проехать {distance}");
 
     }
     class camoletik : Airplane
     {
-        int speed;
-        int capacity;
-        int distance;
-        public camoletik(int speed, int capacity, int distance) : base(speed, capacity, distance) { }
+        public camoletik(int speed, int capacity, int distance){
+            this.speed = speed;
+            this.capacity = capacity;
+            this.distance = distance;
+        }
         public void print() => Console.WriteLine($"Самолётик грузоподъёмностью {capacity} может проехать {distance}");
     }
     class Program
     {
         static void Main(string[] args)
         {
-            camoletik odin = new camoletik(5, 4, 5);
-            bibika dwa = new bibika(15, 2, 1);
-            lodka tree = new lodka(7, 6, 3);
+            camoletik odin = new camoletik(5, 15, 200);
+            bibika dwa = new bibika(15, 2, 100);
+            lodka tree = new lodka(7, 6, 30);
             odin.print();
             odin.speedinf();
             dwa.print();
